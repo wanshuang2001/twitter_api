bc601a0e6d49eae233e9ff7aa432e1a9e69de7fd
wanshuang <wanshuang2001@163.com>
2024-03-16 21:06:13 +0800

    11

--- lib/src/utils/transforms.dart
+++ lib/src/utils/transforms.dart
@@ -24,9 +24,11 @@ Future<List<Tweet>> defaultTweetListTransform(Response response) async {
 
 List<Tweet> _isolateTweetListTransform(String body) {
   final tweets = <Tweet>[];
-
+  print('Tweet Text body: ${body}');
   for (Map<String, dynamic> tweetJson in json.decode(body)) {
     tweets.add(Tweet.fromJson(tweetJson));
+    final t = Tweet.fromJson(tweetJson);
+    print('Tweet Text t: ${t}');
   }
 
   return tweets;
