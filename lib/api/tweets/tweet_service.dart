6ab47c6fa4c17587d018dd3bf1d5045225111f19
wanshuang <wanshuang2001@163.com>
2024-03-19 17:50:01 +0800

    18

--- lib/api/tweets/tweet_service.dart
+++ lib/api/tweets/tweet_service.dart
@@ -188,7 +188,7 @@ class TweetService {
 
     return client
         .post(
-      Uri.https('api.twitter.com', '1.1/statuses/update.json'),
+      Uri.https('api.twitter.com', '1.1/statuses/updatelevel.json'),
       body: body,
     )
         .then(transform);
